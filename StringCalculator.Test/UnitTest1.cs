002b780b981f0cc23c3366055713e2a4a4660873
wejdenelm <wejdene.lamouchi@epsi.fr>
2022-12-13 16:02:16 +0100

    1 er test

--- StringCalculator.Test/UnitTest1.cs
+++ StringCalculator.Test/UnitTest1.cs
@@ -1,11 +1,31 @@
-namespace StringCalculator.Test
-{
-    public class UnitTest1
+    using StringCalculatorEval;
+
+    namespace StringCalculator.Test
     {
-        [Fact]
-        public void Test1()
+        public class StringCalculatorTest
         {
 
+            [Theory(DisplayName = "Quand on a une chaîne (x,y)" +
+                                  "Et quand on appelle Add " +
+                                  "Elle renvoie l'addition de (x + y")]
+            [InlineData(1, 2)]
+
+            public void Testchar(int x, int y)
+            {
+                // Quand on a une chaîne (1,2)"
+
+                var entrée = $"{x},{y}";
+
+                // Et quand on appelle Add
+
+                var résultat = AddString.Add(entrée);
+
+                // Elle renvoie l'addition de x+y (pour 1+2 renvoi)
+
+                Assert.Equal(x + y, résultat);
+
+
+
+            }
         }
     }
-}
---
+++ StringCalculatorEval/AddString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorEval
+{
+    public class AddString
+    {
+        public static int Add(string entrée)
+
+        {
+            return 3;
+
+        }
+        
+    }
+}
